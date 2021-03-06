f66a9102b39a1a6d0249831c571d57b8034d0243
p.sumara <sumara0498@icloud.com>
2021-03-06 16:05:47 +0100

    Add watch section with slide effect

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+// Common JS
+document.querySelectorAll('.watch-control, .controls a, .iphone-btn').forEach(control => {
+    control.addEventListener('click', e => {
+        e.preventDefault()
+    })
+})
+    
+// End of Common JS
+
 // Cube
 let y = 0
 const cube = document.querySelector('.cube')
@@ -79,3 +88,62 @@ window.addEventListener('scroll', () => {
     }
 })
 // End of Section 3
+
+// Section 4
+const watchBands = document.querySelector('.watch-bands')
+const watchCases = document.querySelector('.watch-cases')
+
+const watchTopControl = document.querySelector('.watch-top-control')
+const watchRightControl = document.querySelector('.watch-right-control')
+const watchBottomControl = document.querySelector('.watch-bottom-control')
+const watchLeftControl = document.querySelector('.watch-left-control')
+
+let axisY = 0
+let axisX = 0
+
+const hideControl = () => {
+    if(axisY === -280) {
+        watchTopControl.classList.add('hideControl')
+    } else {
+        watchTopControl.classList.remove('hideControl')
+    }
+
+    if(axisY === 280) {
+        watchBottomControl.classList.add('hideControl')
+    } else {
+        watchBottomControl.classList.remove('hideControl')
+    }
+
+    if(axisX === 280) {
+        watchRightControl.classList.add('hideControl')
+    } else {
+        watchRightControl.classList.remove('hideControl')
+    }
+
+    if(axisX === -280) {
+        watchLeftControl.classList.add('hideControl')
+    } else {
+        watchLeftControl.classList.remove('hideControl')
+    }
+}
+
+watchTopControl.addEventListener('click', () => {
+    watchCases.style.marginTop = `${axisY -= 70}rem`
+    hideControl()
+})
+
+watchBottomControl.addEventListener('click', () => {
+    watchCases.style.marginTop = `${axisY += 70}rem`
+    hideControl()
+})
+
+watchRightControl.addEventListener('click', () => {
+    watchBands.style.marginRight = `${axisX += 70}rem`
+    hideControl()
+})
+
+watchLeftControl.addEventListener('click', () => {
+    watchBands.style.marginRight = `${axisX -= 70}rem`
+    hideControl()
+})
+// End of Section 4
